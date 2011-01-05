9f9cf04fae178bcacff459df5645ac2add15c792
cmilfont <cmilfont@gmail.com>
2011-01-05 18:41:40 -0300

    não lembro

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,8 @@
+test("Um teste simples", function() {
+  var objeto = Object.create({test:10})
+  ok( objeto.trying("test") == 10, "" );
+});
+
 test("Propriedades com objetos aninhados", function() {
   var objeto = Object.create({test:{test2:{method: {id:10} }}})
   ok( objeto.trying("test.test2.method.id") == 10, "" );
--- teste.js
+++
@@ -1,4 +0,0 @@
-require("./lib/trying.js");
-var objeto = Object.create({test:{test2:{method: function() { return {id:10}} }}})
-console.log(objeto.trying("test.test2.method.id") == 10);
-
