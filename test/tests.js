4fb68762a6796287c18f9c295791c3c943e45a04
cmilfont <cmilfont@gmail.com>
2011-01-06 09:08:02 -0300

    mais testes para triangular

--- test/tests.js
+++ test/tests.js
@@ -18,3 +18,18 @@ test("Propriedades com funções aninhadas", function() {
   ok( objeto.trying("test.test2.method.id") == 10, "" );
 });
 
+test("Propriedades com funções nulas", function() {
+  var objeto = Object.create({test:{test2:{method: function() { return null} }}})
+  ok( objeto.trying("test.test2.method.id") != 10, "" );
+});
+
+test("Propriedades com funções booleanas", function() {
+  var objeto = Object.create({test:{test2:{method: function() { return true} }}})
+  ok( objeto.trying("test.test2.method.id") != 10, "" );
+});
+
+test("Propriedades com funções undefined", function() {
+  var objeto = Object.create({test:{test2:{method: function() { return undefined} }}})
+  ok( objeto.trying("test.test2.method.id") != 10, "" );
+});
+
