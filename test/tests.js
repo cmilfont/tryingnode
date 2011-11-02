e2655d5232e74dee319cf96015e4851e6bce9241
Christiano Milfont <cmilfont@gmail.com>
2011-11-02 09:00:24 -0300

    refactoring after course

--- test/tests.js
+++ test/tests.js
@@ -1,35 +1,23 @@
-test("Tentar conteúdo de uma propriedade sem lançar exceção", function() {
-  var objeto = Object.create({test:10})
-  ok( objeto.trying("test") == 10, "" );
+test("Tentar conteúdo de uma propriedade", function() {
+  var objeto = Object.create({test:10});
+  ok( objeto.trying("test") === 10, "" );
 });
 
 test("Propriedades com objetos aninhados", function() {
-  var objeto = Object.create({test:{test2:{method: {id:10} }}})
-  ok( objeto.trying("test.test2.method.id") == 10, "" );
+  var objeto = Object.create({test:{test2:{method: {id:10} }}});
+  ok( objeto.trying("test.test2.method.id") === 10, "" );
 });
 
-test("Propriedades com função", function() {
-  var objeto = Object.create({test:{test2:{method: function() { return {id:10}} }}})
-  ok( objeto.trying("test.test2.method.id") == 10, "" );
+test("Propriedades falsa de objetos aninhados", function() {
+  var objeto = Object.create({test:{test2:{method: {id:10} }}});
+  ok( objeto.trying("test.test.method.id") !== 10, "" );
 });
 
-test("Propriedades com funções aninhadas", function() {
-  var objeto = Object.create({test:{test2:{method: function() { return {id:function(){return 10} }} }}})
-  ok( objeto.trying("test.test2.method.id") == 10, "" );
+test("Verificar condicional a partir do trying", function() {
+  var objeto = Object.create({test:{test2:{method: {name:"Test Success"} }}});
+  if( objeto.trying("test.test2.method.name") === "Test Success") {
+  	ok(true);
+  } else {
+  	ok(false);
+  }
 });
-
-test("Propriedades com funções nulas", function() {
-  var objeto = Object.create({test:{test2:{method: function() { return null} }}})
-  ok( objeto.trying("test.test2.method.id") != 10, "" );
-});
-
-test("Propriedades com funções booleanas", function() {
-  var objeto = Object.create({test:{test2:{method: function() { return true} }}})
-  ok( objeto.trying("test.test2.method.id") != 10, "" );
-});
-
-test("Propriedades com funções undefined", function() {
-  var objeto = Object.create({test:{test2:{method: function() { return undefined} }}})
-  ok( objeto.trying("test.test2.method.id") != 10, "" );
-});
-
